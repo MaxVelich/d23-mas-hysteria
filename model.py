0cf61e681d333442eeae605b1783948998fc6e87
MaxVelich <developer@velich.me>
2020-09-13 21:08:36 +0200

    First tutorial of Mesa Docs

---
+++ model.py
@@ -0,0 +1,19 @@
+
+from mesa import Model
+from agent import MoneyAgent
+
+from mesa.time import RandomActivation
+
+class MoneyModel(Model):
+    """A model with some number of agents."""
+    def __init__(self, N):
+        self.num_agents = N
+        self.schedule = RandomActivation(self)
+        # Create agents
+        for i in range(self.num_agents):
+            a = MoneyAgent(i, self)
+            self.schedule.add(a)
+
+    def step(self):
+        '''Advance the model by one step.'''
+        self.schedule.step()
