6a1a0da036df20570ba0e690ab824427049de94d
MaxVelich <developer@velich.me>
2020-10-03 10:46:19 +0200

    Implemented structure for intersection of the walkable mesh

--- src/model/Model_Controller.py
+++ src/model/Model_Controller.py
@@ -3,13 +3,11 @@ from src.model.entities.Person import Person
 from src.model.entities.Exit import Exit
 from src.model.entities.Obstacle import Obstacle
 
+from src.model.logic.Path_Finder import Path_Finder
+
 from mesa import Model
 from mesa.time import RandomActivation
 from mesa.space import ContinuousSpace
-
-import numpy as np
-import matplotlib.pyplot as plt
-from scipy.spatial import Delaunay
 
 class Model_Controller(Model):
 
@@ -23,40 +21,25 @@ class Model_Controller(Model):
         self.create_exit()
         self.create_agent()
 
-        points = [
-            (0,0), (0, 99), (0, 199), (0, 299), (0, 399), (0, 499),
-            (99, 0), (199, 0), (299, 0), (399, 0), (499, 0),
-            (99, 499), (199, 499), (299, 499), (399, 499), (499, 499),
-            (499,0), (499, 99), (499, 199), (499, 299), (499, 399)
-        ]
-        for obstacle in self.obstacles:
-            points += obstacle.get_corner_points()
-
-        points = np.array(points)
-        tri = Delaunay(points) 
-        
-        plt.triplot(points[:,0], points[:,1], tri.simplices)
-        plt.plot(points[:,0], points[:,1], 'o')
-        plt.show()
+        self.path_finder = Path_Finder((width, height), self.obstacles, self.exits)
+        self.path_finder.build_mesh()
 
     def create_agent(self):
         for i in range(self.num_agents):
             a = Person(i, self)
-
-            self.space.place_agent(a, (20, 20))
-
+            self.space.place_agent(a, (40, 40))
             self.schedule.add(a)
 
     def create_exit(self):
         self.exits = {
-            Exit(480, 480, True)
+            Exit(40, 460, True)
         }
 
     def create_obstacles(self):
         self.obstacles = {
-            Obstacle((150,100), 150, 100),
-            Obstacle((350,200), 150, 100),
-            Obstacle((110,260), 100, 150)
+            Obstacle((150,100), 300, 10),
+            Obstacle((300,245), 10, 300),
+            Obstacle((200,350), 10, 300)
         }
 
     def step(self):
--- src/model/logic/Path_Finder.py
+++ src/model/logic/Path_Finder.py
@@ -3,14 +3,73 @@
 This class is currently empty. Here we intend to provide the logic for the agents to find a path towards an exit. We intend to not only provide algorithms for the nearest path, but also alternative routes. 
 '''
 
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.spatial import Delaunay
+
 class Path_Finder:
 
-    def __init__(self, world, obstacles, exits):
-        self.world = world
+    def __init__(self, world_dim, obstacles, exits):
+        self.world_dim = world_dim
         self.obstacles = obstacles
         self.exits = exits
 
     def description(self):
-        print("world:", self.world)
+        print("world_dim:", self.world_dim)
         print("obstacles:", self.obstacles)
         print("exits:", self.exits)
+
+    def build_mesh(self):
+
+        points = [
+            (0,0), (0, 99), (0, 199), (0, 299), (0, 399), (0, 499),
+            (99, 0), (199, 0), (299, 0), (399, 0), (499, 0),
+            (99, 499), (199, 499), (299, 499), (399, 499), (499, 499),
+            (499,0), (499, 99), (499, 199), (499, 299), (499, 399)
+        ]
+        # points = []
+        for obstacle in self.obstacles:
+            points += obstacle.get_corner_points()
+
+        points = np.array(points)
+        tri = Delaunay(points)
+
+        walkable_space = self.filter_triangles_leaving_walkable_space(tri.simplices, self.obstacles)
+        print(walkable_space)
+        
+        # plt.triplot(points[:,0], points[:,1], tri.simplices)
+        # plt.plot(points[:,0], points[:,1], 'o')
+        # plt.show()
+
+    def filter_triangles_leaving_walkable_space(self, triangles, obstacles):
+        
+        filtered = np.empty([0,3])
+        for triangle in triangles:
+            for obstacle in obstacles:
+                if not self.triangle_intersects_with_obstacle(triangle, obstacle):
+                    filtered = np.append(filtered, triangle.reshape((-1,3)), axis = 0)
+                else:
+                    break
+        
+        return filtered
+
+
+    def triangle_intersects_with_obstacle(self, triangle, obstacle):
+
+        for index, point in np.ndenumerate(triangle):
+            
+            corners_of_obstacle = obstacle.get_corner_points()
+
+            if self.point_lies_in_rectangle(point, corners_of_obstacle):
+                return True
+
+            if self.edge_intersects_with_rectangle_edges((point, triangle[index[0] % 3]), corners_of_obstacle):
+                return True
+
+        return False
+
+    def point_lies_in_rectangle(self, point, rectangle):
+        return False
+
+    def edge_intersects_with_rectangle_edges(self, edge, rectangle):
+        return False
