9be61ed82a989d4893d6c502d91f79fb344a6aee
prajaktashouche <prajakta.90199@gmail.com>
2020-10-04 21:17:49 +0200

    fixed issue with exit

--- src/model/entities/Exit.py
+++ src/model/entities/Exit.py
@@ -3,9 +3,12 @@
 This class models the Exit object. This is the objects agents want to rush to in case of an emergency.
 '''
 
+
 class Exit:
 
     def __init__(self, x = 0, y = 0):
         self.x = x
         self.y = y
         self.pos = (x, y)
+        self.available = True
+
