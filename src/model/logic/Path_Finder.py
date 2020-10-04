099df3ef24da5c8c599dbca4c7c8f5dd18ac842a
MaxVelich <developer@velich.me>
2020-10-04 19:13:18 +0200

    Commented on Path_Finder

--- src/model/logic/Path_Finder.py
+++ src/model/logic/Path_Finder.py
@@ -1,15 +1,15 @@
 
 '''
+This class is responsible for managing the path finding of the agents. We discretize/quantize the continuous space here, and then run A* on the resulting walkable (since we have obstacles) graph.
 
+This class still has a long way to go! Currently, we recalculate the route for each agent after each step -> Very bad for performance
 '''
 
-import numpy as np
-import matplotlib.pyplot as plt
-from scipy.spatial import Delaunay
-import sys
 from src.model.logic.A_star import A_Star
 from src.model.utils.Utilities import Utilities
 from src.model.utils.Geometry import Geometry
+
+import numpy as np
 
 class Path_Finder:
 
@@ -22,17 +22,28 @@ class Path_Finder:
     ### PUBLIC INTERFACE
 
     def build_mesh(self):
+        '''
+        In order to run A* on the world, we need to make state spaces discrete. We use Delaunay's algrithm in order to find triangles across all nodes. Then, we filter those triangle depending on if they fall on walkable space or not.
+        '''
 
         self.nodes = self.__prepare_nodes_of_graph()
+
+        from scipy.spatial import Delaunay
         triangles = Delaunay(self.nodes)
+
         self.walkable_space = self.__find_walkable_space(triangles.simplices, self.obstacles)
 
+        # UNCOMMENT IF YOU WANT TO SEE THE MESH OF THE GRAPH
+        # import matplotlib.pyplot as plt
         # self.nodes = np.array(self.nodes)
         # plt.triplot(self.nodes[:,0], self.nodes[:,1], self.walkable_space)
         # plt.plot(self.nodes[:,0], self.nodes[:,1], 'o')
         # plt.show()
 
     def get_next_step(self, agent_position, goal = (20, 480)):
+        '''
+        This will not make it in the final version. It produces the next step an agent should take in order to find an exit. Here, we basically run A* on the graph we generate above. Then we try to match the agent's position to the next nearest node on the graph, then we run A*.
+        '''
 
         nearest_point = self.__find_nearest_mesh_point(agent_position)
 
@@ -52,6 +63,10 @@ class Path_Finder:
     ### PRIVATE INTERFACE
 
     def __prepare_nodes_of_graph(self):
+        '''
+        Here we setup all nodes we need for the graph. We use 'artificial' nodes to cover empty space, the corner points of rectangle obstacles, and the exits' positions.
+        '''
+
         nodes = []
 
         for x in range(0, int(self.world_dim[0] / self.mesh_size) + 1):
@@ -67,6 +82,9 @@ class Path_Finder:
         return nodes
 
     def __find_walkable_space(self, triangles, obstacles):
+        '''
+        If one of the triangles intersects with an obstacle we need to remove it so that the agent can't walk over it -- like a ghost. The two methods following this one are helper functions.
+        '''
         
         filtered = np.empty([0,3])
         for triangle in triangles:
@@ -100,6 +118,9 @@ class Path_Finder:
         return False
 
     def __find_nearest_mesh_point(self, point):
+        '''
+        We need to first let the agent 'get on the grid'. Therefore, we find the nearest node on the graph here.
+        '''
 
         distances = []
         for p in self.nodes:
@@ -111,11 +132,18 @@ class Path_Finder:
         return self.nodes[index_nearest_point]
 
     def __find_path(self, start, goal):
+        '''
+        Here we run A* on the graph, though A* itself is in a separate class.
+        '''
+
         edges = self.__prepare_edges_for_path_finding()
         a_star = A_Star(edges)
         return a_star.find_path(start, goal)
 
     def __prepare_edges_for_path_finding(self):
+        '''
+        From the Delaunay method, we find triangles. Though, we need to convert these into edges (i.e. [point_1, point_2]). Also, we can remove bidirectional-duplicates, since we we do not have any directed edges (i.e. [point_1,point_2] == [point_2, point_1]).
+        '''
 
         edges = []
         for triangle in self.walkable_space:
